61f25967cfe6867bd609a04f5880faa49e532164
Marcus Nyeholt <marcus@symbiote.com.au>
2017-12-20 10:14:43 +1100

    fix(InteractiveImpression) Moved get_browser_info to private method

--- code/dataobjects/InteractiveImpression.php
+++ code/dataobjects/InteractiveImpression.php
@@ -21,12 +21,10 @@ class InteractiveImpression extends DataObject
         'Referer' => 'Varchar',
         'RemoteIP' => 'Varchar',
     );
-
     private static $has_one = array(
         'User' => 'Member',
         'Interactive' => 'Interactive',
     );
-
     private static $defaults = array(
         'Interaction' => 'View',
     );
@@ -34,7 +32,7 @@ class InteractiveImpression extends DataObject
     public function onBeforeWrite()
     {
         if (!$this->ID) {
-            $browser              = get_browser_info();
+            $browser              = $this->getBrowserInfo();
             $this->UserAgent      = $browser['user_agent'];
             $this->Browser        = $browser['name'];
             $this->BrowserVersion = $browser['version'];
@@ -52,82 +50,82 @@ class InteractiveImpression extends DataObject
 
         parent::onBeforeWrite();
     }
-}
 
-function get_browser_info()
-{
-    $u_agent = $_SERVER['HTTP_USER_AGENT'];
+    protected function getBrowserInfo()
+    {
+        $u_agent = $_SERVER['HTTP_USER_AGENT'];
 // $u_agent = 'MSIE win32';
 // $u_agent = 'mac os x Safari 2';
 // $u_agent = 'mac os x Chrome 8';
 // $u_agent = 'linux Chrome 8';
 
-    $bname    = 'Unknown';
-    $platform = 'Unknown';
-    $version  = "";
+        $bname    = 'Unknown';
+        $platform = 'Unknown';
+        $version  = "";
 
-    //First get the platform?
-    if (preg_match('/linux/i', $u_agent)) {
-        $platform = 'linux';
-    } elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
-        $platform = 'mac';
-    } elseif (preg_match('/windows|win32/i', $u_agent)) {
-        $platform = 'windows';
-    }
-
-    // Next get the name of the useragent yes seperately and for good reason
-    if (preg_match('/MSIE/i', $u_agent) && !preg_match('/Opera/i', $u_agent)) {
-        $bname = 'Internet Explorer';
-        $ub    = "MSIE";
-    } elseif (preg_match('/Firefox/i', $u_agent)) {
-        $bname = 'Mozilla Firefox';
-        $ub    = "Firefox";
-    } elseif (preg_match('/Chrome/i', $u_agent)) {
-        $bname = 'Google Chrome';
-        $ub    = "Chrome";
-    } elseif (preg_match('/Safari/i', $u_agent)) {
-        $bname = 'Apple Safari';
-        $ub    = "Safari";
-    } elseif (preg_match('/Opera/i', $u_agent)) {
-        $bname = 'Opera';
-        $ub    = "Opera";
-    } elseif (preg_match('/Netscape/i', $u_agent)) {
-        $bname = 'Netscape';
-        $ub    = "Netscape";
-    }
-
-    // finally get the correct version number
-    $known   = array('Version', $ub, 'other');
-    $pattern = '#(?P<browser>'.join('|', $known).
-        ')[/ ]+(?P<version>[0-9.|a-zA-Z.]*)#';
-    if (!preg_match_all($pattern, $u_agent, $matches)) {
-        // we have no matching number just continue
-    }
-
-    // see how many we have
-    $i = count($matches['browser']);
-    if ($i != 1) {
-        //we will have two since we are not using 'other' argument yet
-        //see if version is before or after the name
-        if (strripos($u_agent, "Version") < strripos($u_agent, $ub)) {
-            $version = $matches['version'][0];
-        } else {
-            $version = $matches['version'][1];
+        //First get the platform?
+        if (preg_match('/linux/i', $u_agent)) {
+            $platform = 'linux';
+        } elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
+            $platform = 'mac';
+        } elseif (preg_match('/windows|win32/i', $u_agent)) {
+            $platform = 'windows';
         }
-    } else {
-        $version = $matches['version'][0];
-    }
 
-    // check if we have a number
-    if ($version == null || $version == "") {
-        $version = "?";
-    }
+        // Next get the name of the useragent yes seperately and for good reason
+        if (preg_match('/MSIE/i', $u_agent) && !preg_match('/Opera/i', $u_agent)) {
+            $bname = 'Internet Explorer';
+            $ub    = "MSIE";
+        } elseif (preg_match('/Firefox/i', $u_agent)) {
+            $bname = 'Mozilla Firefox';
+            $ub    = "Firefox";
+        } elseif (preg_match('/Chrome/i', $u_agent)) {
+            $bname = 'Google Chrome';
+            $ub    = "Chrome";
+        } elseif (preg_match('/Safari/i', $u_agent)) {
+            $bname = 'Apple Safari';
+            $ub    = "Safari";
+        } elseif (preg_match('/Opera/i', $u_agent)) {
+            $bname = 'Opera';
+            $ub    = "Opera";
+        } elseif (preg_match('/Netscape/i', $u_agent)) {
+            $bname = 'Netscape';
+            $ub    = "Netscape";
+        }
 
-    return array(
-        'user_agent' => $u_agent,
-        'name' => $bname,
-        'version' => $version,
-        'platform' => $platform,
-        'pattern' => $pattern
-    );
+        // finally get the correct version number
+        $known   = array('Version', $ub, 'other');
+        $pattern = '#(?P<browser>'.join('|', $known).
+            ')[/ ]+(?P<version>[0-9.|a-zA-Z.]*)#';
+        if (!preg_match_all($pattern, $u_agent, $matches)) {
+            // we have no matching number just continue
+        }
+
+        // see how many we have
+        $i = count($matches['browser']);
+        if ($i != 1) {
+            //we will have two since we are not using 'other' argument yet
+            //see if version is before or after the name
+            if (strripos($u_agent, "Version") < strripos($u_agent, $ub)) {
+                $version = $matches['version'][0];
+            } else {
+                $version = $matches['version'][1];
+            }
+        } else {
+            $version = $matches['version'][0];
+        }
+
+        // check if we have a number
+        if ($version == null || $version == "") {
+            $version = "?";
+        }
+
+        return array(
+            'user_agent' => $u_agent,
+            'name' => $bname,
+            'version' => $version,
+            'platform' => $platform,
+            'pattern' => $pattern
+        );
+    }
 }
