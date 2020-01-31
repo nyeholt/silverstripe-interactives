e97590304c712e14fbfc8288f1ef1bb0a2684498
jcarter <josh@symbiote.com.au>
2020-01-31 14:39:04 +1100

    new(admin.css): Styling for DropdownField in ToggleCompositeField

--- src/Control/InteractiveAdmin.php
+++ src/Control/InteractiveAdmin.php
@@ -5,20 +5,27 @@ namespace Symbiote\Interactives\Control;
 use Symbiote\Interactives\Model\InteractiveCampaign;
 use Symbiote\Interactives\Model\InteractiveClient;
 use SilverStripe\Admin\ModelAdmin;
+use SilverStripe\View\Requirements;
+use SilverStripe\View\ThemeResourceLoader;
 
 /**
  * @author Marcus Nyeholt <marcus@silverstripe.com.au>
  * @license BSD http://silverstripe.org/BSD-license
  */
 class InteractiveAdmin extends ModelAdmin {
-	private static $managed_models = array(
-//		'Interactive',
+	private static $managed_models = [
+		// 'Interactive',
 		InteractiveCampaign::class,
 		InteractiveClient::class,
-    );
+	];
 
     private static $menu_icon_class = 'font-icon-box';
-
 	private static $url_segment = 'interactives';
 	private static $menu_title = 'Interactives';
+
+    protected function init()
+    {
+        parent::init();
+        Requirements::css('nyeholt/silverstripe-interactives:client/css/admin.css');
+    }
 }
